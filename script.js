c81a1fdf5a18d3785887b4610d9078a0b2b41da4
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2024-06-27 21:44:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import Ball from "./Ball.js"
 import Paddle from "./Paddle.js"
-
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
